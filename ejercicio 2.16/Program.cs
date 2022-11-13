ef89659f6d1a2b7ad0489b812878696b39cc0137
SANTIAGO <SANTIAGO@LAPTOP-REHH1RB2>
2022-11-13 17:22:37 -0600

    Agregar archivos de proyecto.

---
+++ ejercicio 2.16/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace ejercicio216
+{
+    public class ejercicio126
+    {
+        public static void Main()
+        {
+            int contador;
+            int limite;
+            int sumatoria;
+            sumatoria= 0;
+            contador = 1;
+            limite = 10;
+            for (contador =1; contador <= limite;contador++)
+            {
+                sumatoria = sumatoria + contador;
+                Console.WriteLine($"el resultado de la sumatoria de los numeros es {sumatoria}");
+            }
+        }
+    }
+}
